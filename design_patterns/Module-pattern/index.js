7d92df262b2fb168a5a282e6c0df9b7764562536
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-01-07 19:30:01 +0530

    feat: added module pattern

---
+++ design_patterns/Module-pattern/index.js
@@ -0,0 +1,10 @@
+// Split up your code into smaller, reusable pieces
+
+import { add, divide, multiply, subtract } from "./math.js";
+
+const a = 10;
+const b = 20;
+console.log(`addition of ${a} & ${b} is: ${add(a, b)}`);
+console.log(`subtraction of ${a} & ${b} is: ${subtract(a, b)}`);
+console.log(`multiplication of ${a} & ${b} is: ${multiply(a, b)}`);
+console.log(`division of ${a} & ${b} is: ${divide(a, b)}`);
