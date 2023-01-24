d74ebf12bb15a48065142a0a64fd6e7c3f4bc9e8
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-01-24 10:28:15 +0530

    fix: added render-props pattern

---
+++ design_patterns/Render-Props-pattern/ReusableExample.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = props => props.render();
+
+
+render(
+  <div className="App">
+    <Title render={() => <h1>✨ First render prop! ✨</h1>} />
+    <Title render={() => <h2>🔥 Second render prop! 🔥</h2>} />
+    <Title render={() => <h3>🚀 Third render prop! 🚀</h3>} />
+  </div>,
+  document.getElementById('root');
+);
