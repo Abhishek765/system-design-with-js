fb438283bfff7e6ec3835845b68fd7831b22242a
ABHISHEK VISHWAKARMA <43133456+Abhishek765@users.noreply.github.com>
2023-01-24 10:28:41 +0530

    fix: added render-props pattern (#12)

---
+++ design_patterns/Render-Props-pattern/BasicExample.js
@@ -0,0 +1,27 @@
+// Pass JSX elements to components through props
+
+import React from "react";
+import { render } from "react-dom";
+
+import "./styles.css";
+
+const Title = (props) => props.render();
+
+render(
+  <div className="App">
+    <Title
+      render={() => (
+        <h1>
+          <span role="img" aria-label="emoji">
+            ✨
+          </span>
+          I am a render prop!{" "}
+          <span role="img" aria-label="emoji">
+            ✨
+          </span>
+        </h1>
+      )}
+    />
+  </div>,
+  document.getElementById("root")
+);
