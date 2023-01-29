0a681537bf16e6e8cf92b1090158ce831b8dd39d
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-01-29 19:59:04 +0530

    added hooks pattern

---
+++ design_patterns/Hook-pattern/demo.js
@@ -0,0 +1,31 @@
+/* Use functions to reuse stateful logic among multiple components throughout the app */
+
+import React from "react";
+import useKeyPress from "./useKeyPress";
+
+export default function Input() {
+  const [input, setInput] = React.useState("");
+  const pressQ = useKeyPress("q");
+  const pressW = useKeyPress("w");
+  const pressL = useKeyPress("l");
+
+  React.useEffect(() => {
+    console.log(`The user pressed Q!`);
+  }, [pressQ]);
+
+  React.useEffect(() => {
+    console.log(`The user pressed W!`);
+  }, [pressW]);
+
+  React.useEffect(() => {
+    console.log(`The user pressed L!`);
+  }, [pressL]);
+
+  return (
+    <input
+      onChange={(e) => setInput(e.target.value)}
+      value={input}
+      placeholder="Type something..."
+    />
+  );
+}
---
+++ design_patterns/Hook-pattern/useKeyPress.js
@@ -0,0 +1,30 @@
+import React from "react";
+const useKeyPress = (targetKey) => {
+  const [keyPressed, setKeyPressed] = React.useState(false);
+
+  function handleDown({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  }
+
+  function handleUp({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("keydown", handleDown);
+    window.addEventListener("keyup", handleUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleDown);
+      window.removeEventListener("keyup", handleUp);
+    };
+  }, []);
+
+  return keyPressed;
+};
+
+export default useKeyPress;
